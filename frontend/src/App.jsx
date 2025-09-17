a1c8181a69129c112fd3ae287a519983e9f6a8ab
KartikVyas1925 <vyaskartik192@gmail.com>
2025-09-18 00:34:54 +0530

    feat: Set up React, Vite, and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center text-cyan-400 p-4">
+        Insight Trader
+      </h1>
+    </div>
+  )
+}
+
+export default App
